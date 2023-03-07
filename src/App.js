da933fa3bbadd5e6ee5d3baf8ece1e2d99aa4fa5
youjin <w2117@e-mirim.hs.kr>
2023-03-07 15:12:54 +0900

    dom check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+   
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          미림에 오신것을 환영합니다. Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>리액트 안녕😊</h1>
+      <h1>잘 작동하니 ? 👍 </h1>
     </div>
   );
 }
