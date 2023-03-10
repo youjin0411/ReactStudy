414ca7776532d083d7a8f3e98efc0fbb24509466
youjin <w2117@e-mirim.hs.kr>
2023-03-10 11:20:08 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import './App.css';
 import React from 'react';
 function App() {
   const name = '리액트2';
-  return (
-    <div> 
-      { name ==='리액트' ? (
-        <h1> {name} 안녕😘 </h1>
-      ) : (
-        <h2> 잘 작동하니🤷‍♀️ ? </h2>
-      )}
-    </div>
-  );
+  return <div> { name ==='리액트' ? <h1> 리액트입니다. </h1> : null } </div>
 }
 
 export default App;
